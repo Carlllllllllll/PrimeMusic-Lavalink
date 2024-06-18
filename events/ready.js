124c413bf960ed8a7d02aec77500d63352426cb0
Carlllllllllll <158790915+Carlllllllllll@users.noreply.github.com>
2024-06-18 09:30:01 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -45,8 +45,23 @@ console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}
 
 const serverCount = client.guilds.cache.size;
 setInterval(() => client.user.setActivity({ 
-  name:`lavaLink GlaceYT`, 
-  type: ActivityType.Listening }), 10000);
+  name: `Music 🎶`, 
+  type: ActivityType.Listening }), 4000);
+  setInterval(() => client.user.setActivity({ 
+  name: `Your Vibe 🎶`, 
+  type: ActivityType.Listening }), 4000);
+  setInterval(() => client.user.setActivity({ 
+  name: `Your Queue 🎶`, 
+  type: ActivityType.Watching }), 4000);
+    setInterval(() => {
+  const serverCount = client.guilds.cache.size;
+  const memberCount = client.users.cache.size;
+
+  client.user.setActivity({
+    name: `Servers: ${serverCount} | Members: ${memberCount}`,
+    type: ActivityType.Watching
+  });
+}, 4000);
 client.errorLog = config.errorLog
   
 }
